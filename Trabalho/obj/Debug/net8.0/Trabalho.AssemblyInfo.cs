2cb1f177a9a6fd847ab6806900228f61ca11cd96
ArthurYllana <arthur.yllana@gmail.com>
2024-11-01 10:55:08 -0300

    Adição do Trabalho feito em sala

--- Trabalho/obj/Debug/net8.0/Trabalho.AssemblyInfo.cs
+++ Trabalho/obj/Debug/net8.0/Trabalho.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trabalho")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4459d0a3c76d9ccde58af3d9f9ef2772fa89ef3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aab68624b89a08493e023f66699b0639902f785c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trabalho")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trabalho")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
